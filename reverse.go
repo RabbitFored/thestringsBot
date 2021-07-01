122c4ffbbfb703f36fc2966608ac0484c9e48945
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2021-07-01 06:46:14 -0700

    Create reverse.go

---
+++ reverse.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+
+  tl "github.com/goTelegramBot/telepher"
+  "github.com/goTelegramBot/telepher/types"
+)
+
+func reverse(b tl.Bot,m *types.Message){
+
+  var message string
+  if m.ReplyToMessage != nil{
+
+    message = m.ReplyToMessage.Text
+  }else{
+    b.SendMessage(m.Chat.Id,"_Reply to any message with_ /reverse _to reverse texts_",&tl.Options{ParseMode:"Markdown"})
+    return
+  }
+  
+  reverse := reverseString(message)
+  
+  b.SendMessage(m.Chat.Id, reverse,nil)
+
+}
+
+func reverseString(str string) (result string) {
+    for _, v := range str {
+        result = string(v) + result
+    }
+    return
+}
